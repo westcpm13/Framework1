04d01fd1d1d89755a80738b3cfa933f33b6503f1
Pawel Trojan <paweltrojan@MacBook-Pro-Pawel.local>
2017-07-14 07:38:08 +0200

    sample module framework

---
+++ KlasaObjcFramework.m
@@ -0,0 +1,28 @@
+//
+//  KlasaA.m
+//  hhh
+//
+//  Created by Pawel Trojan on 12.07.2017.
+//  Copyright © 2017 Pawel Trojan. All rights reserved.
+//
+
+#import "KlasaObjcFramework.h"
+
+@implementation KlasaObjcFramework
+
+- (void) wypisz {
+    NSLog(@"Dupa maryna");
+}
+
+- (NSString*) zwrocString {
+    return @"zwroc String dupa maryna";
+}
+
+- (NSString*) zwrocPrywatna {
+    PrywatnaFramework * p = [PrywatnaFramework new];
+    return p.zwrocPrywatna;
+}
+
+
+
+@end
